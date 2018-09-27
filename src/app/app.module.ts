d658c90a0879421e567ff9379faa5ca79e870493
Martinez24 <ponchomarpez@gmail.com>
2018-09-27 12:04:06 -0500

    Se agrego el registro de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,23 @@ import { ToastrModule } from "ngx-toastr";
 //firebase
 import { AngularFireModule }  from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
 import { environment } from "../environments/environment";
 
 //components
 import { ProductsComponent } from './components/products/products.component';
 import { ProductListComponent } from './components/products/product-list/product-list.component';
 import { ProductComponent } from './components/products/product/product.component';
-
-//Servicios
-import { ProductService } from "./services/product.service";
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { PrivadoPageComponent } from './components/privado-page/privado-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+
+//Servicios
+import { ProductService } from "./services/product.service";
+import { AuthService } from "./services/auth.service";
 
 //Rutas
 import { AppRoutingModule } from "./app-routing-module";
@@ -47,13 +49,15 @@ import { AppRoutingModule } from "./app-routing-module";
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
+    AngularFireAuthModule,
     FormsModule,
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [
-    ProductService
+    ProductService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
-
-  constructor() { }
+public email: string;
+public password: string;
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
-
+onSubmitaddUser(){
+  this.authService.registerUser(this.email, this.password)
+  .then((res)=>{
+    console.log("Bien!");
+    console.log(res)
+  }).catch((err) =>{
+    console.log(err);
+  });
+}
 }
